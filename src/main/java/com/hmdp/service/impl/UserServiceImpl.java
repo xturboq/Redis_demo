c0e1bd37510fdd3b9ffa1639218818f32cd127bb
zecola <xyk816@gmail.com>
2024-04-06 18:23:48 +0800

    登录功能实现

--- src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -9,9 +9,12 @@ import com.hmdp.mapper.UserMapper;
 import com.hmdp.service.IUserService;
 import com.hmdp.utils.RegexUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.tomcat.util.digester.ObjectCreateRule;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpSession;
+
+import static com.hmdp.utils.SystemConstants.USER_NICK_NAME_PREFIX;
 
 /**
  * <p>
@@ -28,23 +31,56 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
     @Override
     public Result sendCode(String phone, HttpSession session) {
         //1.校验手机号
-        if (RegexUtils.isPhoneInvalid(phone)){
+        if (RegexUtils.isPhoneInvalid(phone)) {
             //2.如果不符合，返回错误信息
-         return Result.fail("手机号格式错误");
+            return Result.fail("手机号格式错误");
         }
         //3.如果符合，生成验证码
         String code = RandomUtil.randomNumbers(6);
         //4.保存验证码到session
-        session.setAttribute("code",code);
+        session.setAttribute("code", code);
         //5.发送验证码
-        log.debug("验证码为：{}",code);
+        log.debug("验证码为：{}", code);
         //6.返回ok
         return Result.ok();
     }
 
+    /**
+     * 用户登录接口
+     * @param loginForm 登录表单，包含手机号和验证码
+     * @param session 用户会话，用于存储和读取验证码、用户信息
+     * @return 登录结果，成功返回成功结果，失败返回失败原因
+     */
     @Override
     public Result login(LoginFormDTO loginForm, HttpSession session) {
+        //1.校验手机号
+        String phone = loginForm.getPhone();
+        if (RegexUtils.isPhoneInvalid(loginForm.getPhone())) {
+            return Result.fail("手机号格式错误");
+        }
+        //2.校验验证码
+        Object cacheCode = session.getAttribute("code");
+        if (cacheCode == null || !cacheCode.toString().equals(loginForm.getCode())) {
+            return Result.fail("验证码错误");
+        }
+        //3.根据手机号查询用户
+        User user = query().eq("phone", phone).one();
+        //4.判断用户是否存在
+        if (user == null) {
+            //5.不存在，创建新用户并保存
+            user = createUserWithPhone(phone);
+        }
+        //6.保存用户到session并返回
+        session.setAttribute("user", user);
+        return Result.ok();
+    }
 
-        return null;
+    private User createUserWithPhone(String phone) {
+        User user = new User();
+        user.setPhone(phone);
+        user.setNickName(USER_NICK_NAME_PREFIX + RandomUtil.randomString(10));
+        //保存用户
+        save(user);
+        return user;
     }
 }
