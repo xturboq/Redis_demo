c6c478c04a0336d0faeea569cf480a2f7ab2100e
zecola <xyk816@gmail.com>
2024-04-06 17:29:24 +0800

    验证码功能实现

---
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -0,0 +1,50 @@
+package com.hmdp.service.impl;
+
+import cn.hutool.core.util.RandomUtil;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.LoginFormDTO;
+import com.hmdp.dto.Result;
+import com.hmdp.entity.User;
+import com.hmdp.mapper.UserMapper;
+import com.hmdp.service.IUserService;
+import com.hmdp.utils.RegexUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpSession;
+
+/**
+ * <p>
+ * 服务实现类
+ * </p>
+ *
+ * @author 虎哥
+ * @since 2021-12-22
+ */
+@Slf4j
+@Service
+public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
+
+    @Override
+    public Result sendCode(String phone, HttpSession session) {
+        //1.校验手机号
+        if (RegexUtils.isPhoneInvalid(phone)){
+            //2.如果不符合，返回错误信息
+         return Result.fail("手机号格式错误");
+        }
+        //3.如果符合，生成验证码
+        String code = RandomUtil.randomNumbers(6);
+        //4.保存验证码到session
+        session.setAttribute("code",code);
+        //5.发送验证码
+        log.debug("验证码为：{}",code);
+        //6.返回ok
+        return Result.ok();
+    }
+
+    @Override
+    public Result login(LoginFormDTO loginForm, HttpSession session) {
+
+        return null;
+    }
+}
